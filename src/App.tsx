e0c019abcf184990f39485d9b342c91af0cb6eb5
zaki <zaki20020302@gmail.com>
2023-02-22 12:40:19 +0900

    refactor: #9 カスタムフック作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { FC, useState } from "react";
-import axios from "axios";
+import { FC } from "react";
 import { UserCard } from "./component/UserCard";
-import { User } from "./types/api/user";
-import { UserProfile } from "./types/UserProfiles";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 export const App: FC = () => {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`,
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+  const onClickFetchUser = () => getUsers();
   return (
     <div>
       <button onClick={onClickFetchUser}>データ取得</button>
---
+++ src/hooks/useAllUsers.ts
@@ -0,0 +1,35 @@
+// 全ユーザー一覧取得
+import { useState } from "react";
+import axios from "axios";
+import { UserProfile } from "../types/UserProfiles";
+import { User } from "../types/api/user";
+
+export const useAllUsers = () => {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getUsers = () => {
+    setLoading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return { getUsers, userProfiles, loading, error };
+};
