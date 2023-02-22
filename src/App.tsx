c8e286fa769669d004eb6943d8b0663e36968a24
zaki <zaki20020302@gmail.com>
2023-02-22 11:27:01 +0900

    feat: #5 ローディング作成

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,24 @@ import { UserProfile } from "./types/UserProfiles";
 
 export const App: FC = () => {
   const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
   const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users").then((res) => {
-      const data = res.data.map((user) => ({
-        id: user.id,
-        name: `${user.name}(${user.username})`,
-        email: user.email,
-        address: `${user.address.city}${user.address.suite}${user.address.street}`,
-      }));
-      setUserProfiles(data);
-    });
+    setLoading(true);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {})
+      .finally(() => {
+        setLoading(false);
+      });
   };
   return (
     <div>
